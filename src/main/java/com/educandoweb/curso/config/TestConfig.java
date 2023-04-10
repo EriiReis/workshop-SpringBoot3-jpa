1ede5fac7864fe74630d5d5954ae8e57bfdcb801
EriiReis <erivelton.r.reis@gmail.com>
2023-04-10 19:23:53 -0300

    Part 1 - Ordem Item, many to many, associacao com atributos

--- src/main/java/com/educandoweb/curso/config/TestConfig.java
+++ src/main/java/com/educandoweb/curso/config/TestConfig.java
@@ -59,15 +59,15 @@ public class TestConfig implements CommandLineRunner{
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3 ,p4 ,p5));
 		
-		User u1 = new User( 1, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-		
-		User u2 = new User( 2, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		User u1 = new User(1, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u2 = new User(2, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		User u3 = new User(3, "Erivelton Reis", "eri@gmail.com", "9889977785", "12548780");
 		
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),u1, OrderStatus.PAID);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.WAITING_PAYMENT);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.WAITING_PAYMENT);
 		
-		userRepository.saveAll(Arrays.asList(u1,u2));
+		userRepository.saveAll(Arrays.asList(u1,u2,u3));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 	}
 
